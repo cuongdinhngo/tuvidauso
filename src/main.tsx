f0d403c4319f449d70d7d9da037cb877f6d277c6
Cuong Ngo <dinhcuongngo@gmail.com>
2026-06-26 20:58:16 +0700

    design: add design tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Self-hosted Vietnamese fonts (no Google Fonts <link> in production)
+import '@fontsource/be-vietnam-pro/400.css'
+import '@fontsource/be-vietnam-pro/500.css'
+import '@fontsource/be-vietnam-pro/600.css'
+import '@fontsource/be-vietnam-pro/700.css'
+import '@fontsource/noto-serif/500.css'
+import '@fontsource/noto-serif/600.css'
+import '@fontsource/noto-serif/700.css'
 import './index.css'
 import App from './App'
 
